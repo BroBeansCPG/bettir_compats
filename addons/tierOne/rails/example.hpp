0dc70c6cb6258df444eb598156baeee93d05fd67
BroBeans <cpgbrobeans@gmail.com>
2026-06-21 21:58:22 +0800

    rails example

---
+++ addons/tierOne/rails/example.hpp
@@ -0,0 +1,17 @@
+class Tier1_Mk48Mod1_Slot: asdg_FrontSideRail
+{
+	class compatibleItems
+	{
+		Tier1_M300C=1;
+		Tier1_M300C_Black=1;
+		Tier1_M600V=1;
+		Tier1_M600V_Black=1;
+		Tier1_Mk48Mod1_LA5=1;
+		Tier1_Mk48Mod1_LA5_M600V=1;
+		Tier1_Mk48Mod1_LA5_M600V_FL=1;
+		Tier1_Mk48Mod1_LA5_M600V_Black=1;
+		Tier1_Mk48Mod1_LA5_M600V_Black_FL=1;
+		Tier1_Mk48Mod1_LA5_M603V=1;
+		Tier1_Mk48Mod1_LA5_M603V_FL=1;
+	};
+};
